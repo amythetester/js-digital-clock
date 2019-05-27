7eacaf428e5a35ba73d16d60262f5d62d52e44dc
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-27 12:57:55 -0700

    removed timezone

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const options = { 
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+};
+
 const setTime = function setTime() {
-    const time = $('<h3>' + Date() + '</h3>');
+    const time = $('<h3>' + Intl.DateTimeFormat('en-US', options).format(new Date()) + '</h3>');
     $('#clock').html(time);
 }
 
-let intervalID = window.setInterval(setTime, 1000);
+window.setInterval(setTime, 1000);
 
 $(document).ready(setTime);
+
