ef1b131a05e50a0a3c603eed7389a592a2081ed2
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-27 12:33:38 -0700

    clock tiks in seconds

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
+const setTime = function setTime() {
     const time = $('<h3>' + Date() + '</h3>');
     $('#clock').html(time);
-})
+}
+
+let intervalID = window.setInterval(setTime, 1000);
+
+$(document).ready(setTime);
